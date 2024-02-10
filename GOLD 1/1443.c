14d805f030de815af6db714875cf5ccd01e539d3
Downcasting <hoo9565j@gmail.com>
2024-02-10 22:15:12 +0900

    first

--- GOLD 1/1443.c
+++ GOLD 1/1443.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void){
-
-
-
+    int D, P;
+    scanf("%d %d", &D, &P);
+    if((int)pow(2, P) >= (int)pow(10, D+1)){
+        printf("-1");
+        return 0;
+    }
+    printf("%d\n",D*P);
 
     return 0;
 }
