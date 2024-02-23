8bc6e928b6e512439f3d6ca4e899587f8d7947c1
Downcasting <hoo9565j@gmail.com>
2024-02-23 20:05:49 +0900

    1509 success

--- CLASS 5/1509.c
+++ CLASS 5/1509.c
@@ -11,7 +11,7 @@
 /* Struct */
 
 /* Global Var */
-int dp[][];
+int** dp;
 char input[2500];
 /* Function */
 int recursive(int start, int end);
@@ -19,24 +19,39 @@ int isPal(int start, int end);
 /* Main */
 int main(void){
     scanf("%s",input);
-
-
+    int length = strlen(input);
+    dp = (int**) malloc(length*sizeof(int*));
+    f(i,length){
+        dp[i] = (int*) malloc(length*sizeof(int));
+        for(int j = i; j < length; j++)
+            dp[i][j] = 0;
+    }
+    long long totalWay = recursive(0,length-1);
+    printf("%lld",totalWay);
     return 0;
 }
 
 /* Functions */
 int recursive(int start, int end){
-    if(end == start)
+    if(end == start){
         return 1;
-    int total = 0;
-    for(int i=start+1; i < end; i++){
-        total += recursive(first) * recursive(second);
     }
-    return total + isPal(str);
+    if(dp[start][end])
+        return dp[start][end];
+    if(isPal(start, end)){
+        return dp[start][end] = 1;
+    }
+    int min = end-start+1;
+    for(int i=0; i < end-start; i++){
+        if(isPal(start, start+i)){
+            min = MIN(min,recursive(start+i+1,end));
+        }
+    }
+    return dp[start][end] = min + 1;
 }
 int isPal(int start, int end){
-    for(int i = start; i < (end - start + 1)/2; i++){
-        if(input[i] != input[end-i])
+    for(int i = 0; i < (end - start + 1)/2; i++){
+        if(input[start+i] != input[end-i])
             return 0;
     }
     return 1;
