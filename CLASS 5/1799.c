eb3f9c2c539b4f077343f4a2da3c6f1677003e4c
Downcasting <hoo9565j@gmail.com>
2024-02-23 20:28:49 +0900

    1799

---
+++ CLASS 5/1799.c
@@ -0,0 +1,35 @@
+/* Include */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Define */
+#define MAX(a,b) a>b?a:b
+#define MIN(a,b) a>b?b:a
+#define f(i,N) for(int i=0; i<N; i++)
+
+/* Struct */
+
+/* Global Var */
+
+/* Function */
+
+/* Main */
+int main(void){
+    int size;
+    scanf("%d",&size);
+
+    int board[size][size];
+    int dp[size][size];
+
+    f(i,size){
+        f(j,size){
+            scanf("%d",&board[i][j]);
+            dp[i][j] = 0;
+        }
+    }
+    for(int i=0; i<size; i++)
+    int count = 0;
+    printf("%d",count);
+    return 0;
+}
+/* Functions */
