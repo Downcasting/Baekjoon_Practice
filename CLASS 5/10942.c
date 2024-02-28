fb73fdab42ff14ec905865445efa91b6b67838c3
Downcasting <hoo9565j@gmail.com>
2024-02-28 12:15:48 +0900

    10942 first try clear!!

--- CLASS 5/10942.c
+++ CLASS 5/10942.c
@@ -13,20 +13,25 @@
 /* Struct */
 
 /* Global Var */
-int aNum;
+int num;
 int* arr;
 int qNum;
 int* qArr;
+int* dp;
 /* Function */
+int question(int a, int b);
 
 /* Main */
 int main(void){
 
-    scanf("%d",&aNum);
-    arr = (int*)malloc(aNum*sizeof(int));
+    scanf("%d",&num);
+    arr = (int*)malloc(num*sizeof(int));
+    dp = (int*)malloc(2*num*sizeof(int));
 
-    f(i,aNum)
+    f(i,num)
         scanf("%d",&arr[i]);
+    f(i,2*num)
+        dp[i] = 0;
     
     scanf("%d",&qNum);
     qArr = (int*)malloc(qNum*sizeof(int));
@@ -34,7 +39,7 @@ int main(void){
     int a,b;
     f(i, qNum){
         scanf("%d %d",&a,&b);
-        qArr[i] = question(a,b);
+        qArr[i] = question(a-1,b-1);
     }
 
     f(i,qNum)
@@ -45,5 +50,20 @@ int main(void){
 
 /* Functions */
 int question(int a, int b){
-    return 1;
+    if(b - a <= 0)                 // 같을 수 밖에 없음
+        return 1;
+
+    if(dp[a+b] >= b-a)              // 이미 정의가 되어 있음
+        return 1;
+    if(arr[a] != arr[b])            // 애초에 이게 양 끝이 다름 -> 0
+        return 0;
+    else{                           // 이게 양 끝이 같다면...
+        if(question(a+1,b-1)){      // 1. 아래쪽도 같으면
+            dp[a+b] = b-a;              // dp 업데이트하기
+            return 1;                   // 옳다고 올려보내기
+        }
+        else                        // 2. 아래쪽은 다르다 -> 0
+            return 0;
+    }
+    return -1;
 }
