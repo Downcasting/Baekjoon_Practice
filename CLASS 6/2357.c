4a94b8a039b250e2aa2c8382c23ee1aa1eec5c99
Downcasting <hoo9565j@gmail.com>
2024-03-11 15:03:55 +0900

    2357

---
+++ CLASS 6/2357.c
@@ -0,0 +1,26 @@
+
+// 2357번 - 최솟값과 최댓값
+
+/* Include */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Define */
+#define MAX(a,b) a>b?a:b
+#define MIN(a,b) a>b?b:a
+#define f(i,N) for(int i=0; i<N; i++)
+
+/* Struct */
+
+/* Global Var */
+
+/* Function Declarations */
+
+/* Main */
+int main(void){
+
+
+    return 0;
+}
+
+/* Functions */
