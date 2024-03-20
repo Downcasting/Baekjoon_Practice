543eef656e9db39e67f56ddf3a0e19c445da3781
Downcasting <hoo9565j@gmail.com>
2024-03-20 18:01:38 +0900

    13334 complete...

--- CLASS 6/13334.c
+++ CLASS 6/13334.c
@@ -9,6 +9,9 @@
 #define MAX(a,b) a>b?a:b
 #define MIN(a,b) a>b?b:a
 #define f(i,N) for(int i=0; i<N; i++)
+#define NULLVAL 999999999
+
+#define PRINTn
 
 /* Struct */
 typedef struct Line{
@@ -19,7 +22,7 @@ typedef struct Line{
 /* Global Var */
 int num;
 Line** lines;
-Line** heap;
+int* heap;
 int heapSize;
 int length;
 
@@ -32,15 +35,19 @@ void addHeap(Line* line);
 int main(void){
     scanf("%d",&num);
     lines = (Line**)malloc(num*sizeof(Line*));
-    heap = (Line**)malloc((num+1)*sizeof(Line*));
+    heap = (int*)malloc((num+1)*sizeof(int));
     int a, b;
     f(i,num){
         scanf("%d %d",&a, &b);
         lines[i] = makeLine(MIN(a,b),MAX(a,b));
-        heap[i+1] = NULL;
+        heap[i+1] = NULLVAL;
     }
     scanf("%d",&length);
     qsort(lines, num, sizeof(Line*),compare);
+    #ifdef PRINT
+    f(i,num)
+        printf("List %d, start: %d end: %d\n",i+1,lines[i]->start, lines[i]->end);
+    #endif
     heapSize = 0;
     int maxSize = 0;
     f(i,num){
@@ -48,6 +55,9 @@ int main(void){
             continue;
         addHeap(lines[i]);
         maxSize = MAX(heapSize, maxSize);
+        #ifdef PRINT
+        printf("%dth Try: heapSize is %d\n",i+1,heapSize);
+        #endif
     }
     printf("%d",maxSize);
     return 0;
@@ -72,45 +82,33 @@ int static compare (const void* first, const void* second)
         else if ((*(Line**)first)->start < (*(Line**)second)->start)
             return -1;
     }
-        return 0;
+    return 0;
 }
 void addHeap(Line* line){
     int curr;
-    while(heap[1] != NULL && line->end - heap[1]->start > length){
+    while(heapSize > 0 && line->end - heap[1] > length){
         heap[1] = heap[heapSize];
-        heap[heapSize--] = NULL;
+        heap[heapSize--] = NULLVAL;
         curr = 1;
-        while(2*curr <= heapSize){
-            int s1 = heap[curr*2] == NULL ? 300000000 : heap[curr*2]->start;
-            int s2 = heap[curr*2+1] == NULL ? 300000000 : heap[curr*2+1]->start;
-            if(MIN(s1,s2) < heap[curr]->start){
-                if(s1 < s2){
-                    Line* temp = heap[curr];
-                    heap[curr] = heap[curr*2];
-                    heap[curr*2] = temp;
-                    curr = curr*2;
-                }
-                else{
-                    Line* temp = heap[curr];
-                    heap[curr] = heap[curr*2+1];
-                    heap[curr*2+1] = temp;
-                    curr = curr*2+1;
-                }
-            }
-            else
+        // 여기가 문제였네;;;;
+        while (2 * curr <= heapSize) {
+            int minChild = (heap[curr * 2] < heap[curr * 2 + 1]) ? curr * 2 : curr * 2 + 1;
+            if (heap[minChild] < heap[curr]) {
+                int temp = heap[curr];
+                heap[curr] = heap[minChild];
+                heap[minChild] = temp;
+                curr = minChild;
+            } else {
                 break;
+            }
         }
     }
-    heap[++heapSize] = line;
+    heap[++heapSize] = line->start;
     curr = heapSize;
-    while(curr > 1){
-        if(heap[curr/2]->start > heap[curr]->start){
-            Line* temp = heap[curr];
-            heap[curr] = heap[curr/2];
-            heap[curr/2] = temp;
-            curr = curr/2;
-        }
-        else
-            break;
+    while (curr > 1 && heap[curr / 2] > heap[curr]) {
+        int temp = heap[curr];
+        heap[curr] = heap[curr / 2];
+        heap[curr / 2] = temp;
+        curr = curr / 2;
     }
 }
