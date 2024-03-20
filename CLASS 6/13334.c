a4dfcd97fd348e97d85eeae4253a03876733579e
Downcasting <hoo9565j@gmail.com>
2024-03-20 10:43:19 +0900

    13334 heap established

--- CLASS 6/13334.c
+++ CLASS 6/13334.c
@@ -20,6 +20,7 @@ typedef struct Line{
 int num;
 Line** lines;
 Line** heap;
+int heapSize;
 int length;
 
 /* Function Declarations */
@@ -28,23 +29,22 @@ int length;
 int main(void){
     scanf("%d",&num);
     lines = (Line**)malloc(num*sizeof(Line*));
-    heap = (Line**)malloc(num*sizeof(Line*));
+    heap = (Line**)malloc((num+1)*sizeof(Line*));
     int a, b;
     f(i,num){
         scanf("%d %d",&a, &b);
         lines[i] = makeLine(MIN(a,b),MAX(a,b));
+        heap[i+1] = NULL;
     }
     scanf("%d",&length);
     qsort(lines, num, sizeof(Line*),compare);
-    int many = 0;
-    int lastStart = lines[0]->start;
-    int lastStartNext;
-    int maxNum = 0;
-    int currentNum = 0;
+    heapSize = 0;
+    int maxSize = 0;
     f(i,num){
         if(lines[i]->end - lines[i]->start > length)
             continue;
-        
+        addHeap(lines[i]);
+        maxSize = MAX(heapSize, maxSize);
     }
     return 0;
 }
@@ -58,15 +58,55 @@ Line* makeLine(int a, int b){
 }
 int static compare (const void* first, const void* second)
 {
-    if ((*(Line**)first)->start > (*(Line**)second)->start)
+    if ((*(Line**)first)->end > (*(Line**)second)->end)
         return 1;
-    else if ((*(Line**)first)->start < (*(Line**)second)->start)
+    else if ((*(Line**)first)->end < (*(Line**)second)->end)
         return -1;
     else{
-        if ((*(Line**)first)->second > (*(Line**)second)->second)
+        if ((*(Line**)first)->start > (*(Line**)second)->start)
             return 1;
-        else if ((*(Line**)first)->second < (*(Line**)second)->second)
+        else if ((*(Line**)first)->start < (*(Line**)second)->start)
             return -1;
     }
         return 0;
+}
+void addHeap(Line* line){
+    int curr;
+    while(line->end - heap[1]->start > length && heap[1] != NULL){
+        heap[1] = heap[heapSize];
+        heap[heapSize--] = NULL;
+        curr = 1;
+        while(2*curr <= heapSize){
+            int s1 = heap[curr*2] == NULL ? 2147483647 : heap[curr*2]->start;
+            int s2 = heap[curr*2] == NULL ? 2147483647 : heap[curr*2+1]->start;
+            if(MIN(s1,s2) < heap[curr]->start){
+                if(s1 < s2){
+                    Line* temp = heap[curr];
+                    heap[curr] = heap[curr*2];
+                    heap[curr*2] = temp;
+                    curr = curr*2;
+                }
+                else{
+                    Line* temp = heap[curr];
+                    heap[curr] = heap[curr*2+1];
+                    heap[curr*2+1] = temp;
+                    curr = curr*2+1;
+                }
+            }
+            else
+                break;
+        }
+    }
+    heap[++heapSize] = line;
+    curr = heapSize;
+    while(curr > 0){
+        if(heap[curr/2]->start > heap[curr]){
+            Line* temp = heap[curr];
+            heap[curr] = heap[curr/2];
+            heap[curr/2] = temp;
+            curr = curr/2;
+        }
+        else
+            break;
+    }
 }
