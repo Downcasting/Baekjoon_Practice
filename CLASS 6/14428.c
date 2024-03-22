99dd570ade01411759c03aaf7339001e30a6be01
Downcasting <hoo9565j@gmail.com>
2024-03-22 11:13:26 +0900

    14428

---
+++ CLASS 6/14428.c
@@ -0,0 +1,26 @@
+
+// 14428번 - 수열과 쿼리 16
+
+/* Include */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Define */
+#define MAX(a,b) a>b?a:b
+#define MIN(a,b) a>b?b:a
+#define f(i,N) for(int i=0; i<N; i++)
+
+/* Struct */
+
+/* Global Var */
+
+/* Function Declarations */
+
+/* Main */
+int main(void){
+
+
+    return 0;
+}
+
+/* Functions */
