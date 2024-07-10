41817a3dbb458780ca6c320d879a0d532464d083
Downcasting <hoo9565j@gmail.com>
2024-07-10 16:56:51 +0900

    startest 17837

---
+++ CLASS 5/17387.c
@@ -0,0 +1,28 @@
+
+// 1234번 - 샘플 코드
+
+/* Include */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Define */
+#define MAX(a,b) a>b?a:b
+#define MIN(a,b) a>b?b:a
+#define f(i,N) for(int i=0; i<N; i++)
+
+/* Struct */
+
+/* Global Var */
+
+/* Function Declarations */
+
+/* Main */
+int main(void){
+    int x1,x2,x3,x4,y1,y2,y3,y4;
+    scanf("%d %d %d %d %d %d %d %d",&x1,&y1,&x2,&y2,&x3,&y3,&x4,&y4);
+
+    return 0;
+}
+
+/* Functions */
