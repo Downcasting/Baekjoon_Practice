7d3f4aa988f469d433d370cd74f5d529224b2bbc
Downcasting <159626883+Downcasting@users.noreply.github.com>
2025-01-06 17:02:23 +0900

    asdfsdf

--- CLASS 6/16287.py
+++ CLASS 6/16287.py
@@ -16,3 +16,10 @@ import sys
 
 # 여기서부터 메인 코드가 시작됨
 
+weight = int(input())
+element_count = int(input())
+element = [int(input()) for _ in range(element_count)]
+
+print('weight is '+weight)
+print('element is '+element)
+print(element)
